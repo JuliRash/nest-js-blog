1c15bbd2012df96446d97acf189955284dcfb7d3
Idowu Julius <idowujulius92@gmail.com>
2021-12-27 02:11:57 +0100

    add post module and configure typeorm for repositories.

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesRepository } from './categories.repository';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { PostsRepository } from './posts.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostsRepository, CategoriesRepository])],
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
 })
 export class PostModule {}
