e80a4fe261140661be52433a1f30acebb317f9cc
Idowu Julius <idowujulius92@gmail.com>
2021-12-27 02:04:52 +0100

    add swagger documentation to root endpoint.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Simple Blog Application')
+    .setDescription('APIs for the simple blog application.')
+    .setVersion('1.0.0')
+    .setTermsOfService('http://swagger.io/terms/')
+
+    .setLicense(
+      'Apache 2.0',
+      'http://www.apache.org/licenses/LICENSE- 2.0.html',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 bootstrap();
