5ace397fc3350a338bcf17209471c68589c861ea
Idowu Julius <idowujulius92@gmail.com>
2021-12-27 02:14:28 +0100

    add swagger api property to user dto.

--- src/auth/user.dto.ts
+++ src/auth/user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsAlphanumeric, IsNotEmpty, MinLength } from 'class-validator';
 
 export class AuthUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsAlphanumeric()
   @MinLength(8)
   @IsNotEmpty()
